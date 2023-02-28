b6d565bda048432d582bc64f0edfa6aa195e167b
pwp114514 <117086223+pwp114514@users.noreply.github.com>
2023-02-28 11:54:23 +0800

    Update ShadersHandler.hx

--- source/ShadersHandler.hx
+++ source/ShadersHandler.hx
@@ -15,3 +15,31 @@ class ShadersHandler
 		chromaticAberration.shader.data.bOffset.value = [chromeOffset * -1];
 	}
 }
+class ChromaticAberration extends FlxShader
+{
+	@:glFragmentSource('
+		#pragma header
+
+		uniform float rOffset;
+		uniform float gOffset;
+		uniform float bOffset;
+
+		void main()
+		{
+			vec4 col = vec4(1.0);
+			
+			col.r = texture2D(bitmap, openfl_TextureCoordv - vec2(rOffset, 0.0)).r;
+			col.ga = texture2D(bitmap, openfl_TextureCoordv - vec2(gOffset, 0.0)).ga;
+			col.b = texture2D(bitmap, openfl_TextureCoordv - vec2(bOffset, 0.0)).b;
+
+			gl_FragColor = col;
+		}')
+	public function new()
+	{
+		super();
+
+		rOffset.value = [0.0];
+		gOffset.value = [0.0];
+		bOffset.value = [0.0];
+	}
+}
